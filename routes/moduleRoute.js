ab3570d7576df9d501274ad19593d98c09380959
KietUTE2812 <21110517@student.hcmute.edu.vn>
2024-11-22 15:15:56 +0700

    Add Tags and update courseId to _id

--- routes/moduleRoute.js
+++ routes/moduleRoute.js
@@ -1,11 +1,14 @@
-﻿// import express from "express";
-// import quizController from "../controllers/quizController.js";
-// // import videoController from "../controllers/videoController.js";
-// import {isLoggedin} from "../middlewares/isLoggedin.js";
-// import {authorize} from "../middlewares/auth.js";
-// import upload from "../config/fileUpload.js";
+﻿import express from "express";
+import quizController from "../controllers/quizController.js";
+// import videoController from "../controllers/videoController.js";
+import {isLoggedin} from "../middlewares/isLoggedin.js";
+import {authorize} from "../middlewares/auth.js";
+import upload from "../config/fileUpload.js";
+import { getModuleById } from "../controllers/moduleController.js"
 
-// const router = express.Router();
+const router = express.Router();
 // router.post('/:moduleId/videos',isLoggedin, authorize('instructor'), upload.uploadMiniO.single('video'), videoController.createVideo);
 
-// export default router;
+router.route('/:id').get(isLoggedin, getModuleById);
+
+export default router;
