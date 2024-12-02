a495ca775b3ea96326a02b87aee2420962d95318
thanhbinh1411 <thanhbinhdang1411@gmail.com>
2024-12-02 16:48:16 +0700

    progress programming when click submit code

--- routes/progressRoute.js
+++ routes/progressRoute.js
@@ -1,11 +1,14 @@
 import progressController from "../controllers/progressController.js";
 import express from "express";
-import {isLoggedin} from "../middlewares/isLoggedin.js";
+import { isLoggedin } from "../middlewares/isLoggedin.js";
+import { authorize, protect } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 // router.route('/').get(isLoggedin, progressController.getProgress);
 router.route('/:id/video').put(isLoggedin, progressController.updateVideoProgress);
 router.route('/:id/supplement').put(isLoggedin, progressController.updateSupplementProgress);
-
+router.route('/:id/programming')
+    .get(protect, authorize("student"), progressController.getProgrammingProgressByProblemId)
+    .put(protect, authorize('student'), progressController.updateProgrammingProgress);
 export default router;
